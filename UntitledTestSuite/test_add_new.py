cfa2a320f150b4ecfcaabf2a73173ebc385f22c6
egork <kegor5619@gmail.com>
2022-06-24 18:23:42 +0300

    Создал вспомогательные методы с помощье refactor-extract method

--- UntitledTestSuite/test_add_new.py
+++ UntitledTestSuite/test_add_new.py
@@ -17,21 +17,23 @@ class TestAddNew(unittest.TestCase):
 
     def test_add_new(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open new add
-        wd.find_element_by_link_text("add new").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_add_new(wd)
+        self.create_new_contacts(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def create_new_contacts(self, wd):
         # init contacts creation
         wd.find_element_by_name("firstname").click()
-        # fill contacts firm
+        # fill contacts form
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("wrtertfe")
         wd.find_element_by_name("middlename").click()
@@ -101,11 +103,22 @@ class TestAddNew(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("iblhbhjvkj")
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        # return to home page
-        wd.find_element_by_link_text("home page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def open_add_new(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
