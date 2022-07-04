63a7e68457fc3031eaf46922364a3e26e5399750
egork <kegor5619@gmail.com>
2022-07-04 18:38:08 +0300

    Создал интеллектуальную фикстуру с проверкой валидности Переместил логин и логаут из тестов в conftest Убрал фикстуру из теста контактов тем самым тесты теперь запускаются в одной сессиии

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contacts import Contact
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_new(app):
-    app.session.login(username="admin", password="secret")
     app.contacts.create_new(Contact(firstname="wrtertfe", middlename="swdwdef", lastname="efwafrar", nickname="rfarfwefa", title="gbfshsrt", company="rragsrehgrt",
                                     address= "sdgthebtbgs", home_number="dfbsthet", mobile_number="dfvrvgr", work_number="scecdc", fax="ececcsc", email="scwferfrcd", email2="scweferv",
                                     email3= "cdevrvdc", homepage= "scefefcs", bday= "11", bmonth= "August", byear= "5655", aday= "13", amonth= "September", ayear= "677", address2= "uykyg",
                                     phone2="yglugyg", notes= "iblhbhjvkj"))
-    app.session.logout()
 
 
 def test_add_null_new(app):
-    app.session.login(username="admin", password="secret")
     app.contacts.create_new(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                     address="", home_number="", mobile_number="", work_number="", fax="", email="", email2="",
                                     email3="", homepage="", bday="11", bmonth= "August", byear= "5655", aday= "13", amonth= "September", ayear= "677", address2= "",
                                     phone2="", notes=""))
-    app.session.logout()
