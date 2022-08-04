7773d639d7780321bd0de2e05e1a0301b4f54639
egork <kegor5619@gmail.com>
2022-08-04 12:47:15 +0300

    Добавил сравнение списков контактов в тес модификации

--- model/contacts.py
+++ model/contacts.py
@@ -32,14 +32,14 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id,self.firstname)
+        return "%s:%s" % (self.id, self.firstname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
 
-    def id_or_max(contact):
-        if contact.id:
-            return int(contact.id)
+    def id_or_max(ct):
+        if ct.id:
+            return int(ct.id)
         else:
             return maxsize
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -8,12 +8,12 @@ def test_modify_contact_firstname(app):
         app.contacts.create_new(Contact(firstname="test"))
     index = randrange(len(old_contacts))
     contact = Contact(firstname="Egor")
-    contact.id = old_contacts[0].id
+    contact.id = old_contacts[index].id
     app.contacts.modify_contact_by_index(index, contact)
     new_contacts = app.contacts.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = contact
-
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 #def test_modify_contact_lastname(app):
