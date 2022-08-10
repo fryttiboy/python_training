3bd4f51f2b5e1f8fc3ab99b415234187fc07f72b
egork <kegor5619@gmail.com>
2022-08-10 18:32:19 +0300

    Создал тест который проверяет одного случайного контакта

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,10 +1,12 @@
-from random import choice
+from random import randrange
 import re
 
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contacts.get_contact_list()[0]
-    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
+    contacts_list = app.contacts.get_contact_list()
+    index = randrange(len(contacts_list))
+    contact_from_home_page = app.contacts.get_contact_list()[index]
+    contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
